1fce2d25fc02611d6e3d038a4510c42d1a014910
Romel180149 <mosiurrahmanromel180149@gmail.com>
2024-01-08 13:36:19 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-const express= require('express');
-const app= express();
-const cors= require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-const port= process.env.PORT || 5000;
-
-//middleware
-app.use(cors());
-app.use(express.json());
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(cookieParser());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mdwccfk.mongodb.net/?retryWrites=true&w=majority`;
 
-
-
-// mosiurrahmanromel180149
-// l8eWzVqXCQ9YlXou
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://mosiurrahmanromel180149:l8eWzVqXCQ9YlXou@cluster0.mdwccfk.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 const logger = (req, res, next) => {
@@ -54,12 +46,10 @@ const verifyToken = (req, res, next) => {
     next();
   });
 };
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    // Send a ping to confirm a successful connection
+
     const foodCollection = client.db("foodDB").collection("foods");
     const foodRequestCollection = client.db("foodDB").collection("foodRequest");
 
@@ -78,6 +68,7 @@ async function run() {
         })
         .send({ success: true });
     });
+
     app.post("/logout", async (req, res) => {
       const user = req.body;
       console.log("logging out", user);
@@ -96,6 +87,7 @@ async function run() {
       const count = await foodCollection.estimatedDocumentCount();
       res.send({ count });
     });
+
     app.get("/food/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -139,6 +131,7 @@ async function run() {
       const result = await foodCollection.insertOne(newFood);
       res.send(result);
     });
+
     app.put("/food/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -194,19 +187,28 @@ async function run() {
       const result = await foodRequestCollection.deleteOne(queryId);
       res.send(result);
     });
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-// run().catch(console.dir);
+
+
+
+
+
+
+
 run().catch(console.dir);
-app.get('/', (req, res) => {
-    res.send('Food sharing server is running')
-})
+
+app.get("/", (req, res) => {
+  res.send("Food sharing Server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Food sharing is running on port: ${port}`)
-})
+  console.log(`Food sharing Server is running on Port ${port}`);
+});
